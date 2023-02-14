c3282fd8d67be3a91f23d0c24544a7255532510c
hemantRawat <hemantrawat812@gmail.com>
2023-02-15 00:22:16 +0530

    Basic structure done

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser  = require('body-parser');
+
+const {PORT}   = require('./config/serverConfig');
+
+const setupAndStartServer  = () =>{
+    
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+
+    app.listen(PORT,()=>{
+
+        console.log(` Server Started on PORT ${PORT}`);
+    })
+
+}
+
+setupAndStartServer();
